002ecf87531f82f067bb60c5ef75bf23c3df136a
Richard Xu <richard.xu@roadbotics.com>
2019-02-07 12:26:43 -0500

    Finish cipher.rb

--- exercises/1_extra/cipher.rb
+++ exercises/1_extra/cipher.rb
@@ -1,6 +1,6 @@
-def read_file(input_file)
+def read_file(filepath)
   lines = []
-  File.open(input_file, 'r').each_line do |line|
+  File.open(filepath, 'r').each_line do |line|
     lines << line.chomp
   end
   lines
@@ -11,7 +11,7 @@ def generate_key(password)
   ordinal_values.reduce(:+) + 1
 end
 
-def encrypt_file(lines, key)
+def encrypt_lines(lines, key)
   encrypted_file = lines.map do |line|
     line_characters = line.split('')
     new_line_characters = line_characters.map do |char|
@@ -22,13 +22,68 @@ def encrypt_file(lines, key)
   encrypted_file
 end
 
-def generate_encrypted_file(input_file)
-  password = 'bonbon'
-  lines = read_file(input_file)
+def generate_encrypted_file(filepath, password)
+  raise "File at #{filepath} not found" unless File.exist?(filepath)
+
+  lines = read_file(filepath)
   key = generate_key(password)
-  encrypted_file = encrypt_file(lines, key)
-  encrypted_file
+  encrypted_lines = encrypt_lines(lines, key)
+  encrypted_lines.map(&:join)
 end
 
-print(read_file('./test.txt'))
-print(generate_encrypted_file('./test.txt'))
+def write_to_file(lines, output_file)
+  File.open(output_file, 'w') do |file|
+    lines.each do |line|
+      file.write(line)
+      file.write("\n")
+    end
+  end
+end
+
+def decrypt_lines(lines, key)
+  decrypted_file = lines.map do |line|
+    line_characters = line.split('')
+    new_line_characters = line_characters.map do |char|
+      char.ord != 32 ? ((char.ord - key) % 128).chr : char
+    end
+    new_line_characters
+  end
+  decrypted_file
+end
+
+def generate_decrypted_file(filepath, password)
+  lines = read_file(filepath)
+  key = generate_key(password)
+  decrypted_lines = decrypt_lines(lines, key)
+  decrypted_lines.map(&:join)
+end
+
+# write_to_file(generate_encrypted_file('./test.txt'), 'rand.txt')
+# puts decrypt_file('./rand.txt')
+
+# check if the first arg is a number?
+def validate_input(arguments)
+  if arguments.length < 3
+    raise 'Please pass in a password, e or d, and a filepath!'
+  elsif arguments.length > 3
+    raise 'Please pass in only password, e or d (for encrypt or decrypt), and a filepath'
+  elsif !%w[e d].include?(arguments[1])
+    raise "I don't recognize the operation (must be 'e' or 'd')"
+  end
+end
+
+validate_input(ARGV)
+
+password, operation, filepath = ARGV
+
+if operation == 'e'
+  write_to_file(
+    generate_encrypted_file(filepath, password),
+    "#{filepath[0...-4]}_encrypted.txt"
+  )
+elsif operation == 'd'
+  write_to_file(
+    generate_decrypted_file(filepath, password), 
+    "#{filepath[0...-14]}_decrypted.txt"
+  )
+end
